1a525c4d70436536dfd47d8e06fa60931d548aeb
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-27 22:53:44 -0300

    edição no coletor

--- coletor.py
+++ coletor.py
@@ -13,12 +13,48 @@ def parsear_linha_auth(linha):
     linha_limpa = linha.strip()
     partes = linha_limpa.split()
 
-    timestamp = f"{partes[0]}, {partes[1]}"
+    if len(partes) != 5:
+        print(f"[AVISO] Formato de log não aceito")
+        return None
 
+    if not partes[3].startswith("usuario="):
+        print(f"[AVISO] Campo de IP inválido: {linha_limpa}")
+        return None
+
+    if not partes[4].startswith("ip="):
+        print(f"[AVISO] Campo de IP inválido: {linha_limpa}")
+        return None
+
+    timestamp = f"{partes[0]}, {partes[1]}"
+    partes_ip = partes[4].split("=")
+    tipo = partes[2]
+    detalhes = partes[3]
+
+    print(detalhes)
+    print(tipo)
+    ip = partes_ip[1]
     print(timestamp)
 
-linha_teste = "2025-02-20 08:15:01 FAIL usuario=admin ip=185.220.101.1"
-parsear_linha_auth(linha_teste)
+    evento = {
+        "timestamp": timestamp,
+        "fonte": "auth",
+        "tipo": tipo,
+        "ip": ip,
+        "detalhes": detalhes,
+        "linha_original": linha_limpa
+    }
+
+    return evento
+
+linha_valida = (
+    "2025-02-20 08:15:01 FAIL "
+    "usuario=admin ip=185.220.101.1"
+)
+
+linha_invalida = "linha sem formato"
+
+print(parsear_linha_auth(linha_valida))
+print(parsear_linha_auth(linha_invalida))
 
 def parsear_linha_firewall(linha):
     """Parseia uma linha do firewall.log e retorna dict normalizado."""
